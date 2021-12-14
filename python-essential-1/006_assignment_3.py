bf11cd592db13073d1950e1710aff32dd1b0d1a0
jatingarg15111 <95943534+jatingarg15111@users.noreply.github.com>
2021-12-13 22:17:04 -0500

    Update 006_assignment_3.py

--- python-essential-1/006_assignment_3.py
+++ python-essential-1/006_assignment_3.py
@@ -1,6 +1,6 @@
 T_h=int(input("Give me the start time(hour): "))
 T_m=int(input("Give me the start time (minute): "))
 T_d=int(input("Give me the duration in minutes: "))
-T_d=(T_h*60)+T_m+T_d
+T_r=(T_h*60)+T_m+T_d
 print("The finish time is: ",end="")
-print(T_d//60,T_d%60,sep=":")
+print(T_r//60,T_r%60,sep=":")
